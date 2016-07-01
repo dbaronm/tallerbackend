ec900520dbf9d6a7b7cda25e23d506706b052881
David <david.baron@softtek.com>
2016-06-30 20:13:40 -0500

    users relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 	validates :title, :name, :username, :email, presence: true 
 	validates :username, :email, uniqueness: true
+	has_many :lists, dependent: :destroy
 end
